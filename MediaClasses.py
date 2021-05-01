f6176b0d8830aa9f52e4ce2892577ec2b5b28820
lkdoe <leonhard.doerflinger@gmail.com>
2021-05-01 18:56:25 +0200

    First Steps with the media classes

---
+++ MediaClasses.py
@@ -0,0 +1,9 @@
+class media:
+    def __init__(self, title):
+        self.title = title
+
+    def set_title(self, title):
+        self.title = title
+    
+    def get_title(self)
+        return self.title
