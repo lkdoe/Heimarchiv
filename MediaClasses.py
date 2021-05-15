916fa7afa40c30e2af2ffb3ce02d9d7d72ae5376
lkdoe <leonhard.doerflinger@gmail.com>
2021-05-15 18:19:28 +0200

    MediaClasses has been reduced to one class which has media type as an attribute

--- MediaClasses.py
+++ MediaClasses.py
@@ -1,9 +1,36 @@
 class Media:
     def __init__(self, title):
         self.title = title
+        self.author = ""  # For books
+        self.director = ""  # For Movies
+        self.year = ""
+        self.medium = ""  # Media type
+        self.genre = ""
+        self.subgenre = ""
+        self.players = 0  # For games
+        self.age_restriction = None
+        self.actors = []
 
-    def set_title(self, title):
-        self.title = title
+    def set_attr(self, key, value):
+        self.__dict__[key] = value
 
-    def get_title(self):
-        return self.title
+    def get_attr(self, key):
+        return self.__dict__[key]
+
+    def __del__(self):
+        delstatement = str(self.title + " has been deleted.")
+        print(delstatement)
+        return delstatement
+
+
+# Test:
+rh = Media("Der Räuber Hotzenplotz")
+
+print("Titel: ", rh)
+
+rh.set_attr('author', 'Ottfried Preußler')
+
+print(rh.title, "ist von", rh.author)
+for item in rh.__dict__.items():
+    print(item)
+print("Ende")
